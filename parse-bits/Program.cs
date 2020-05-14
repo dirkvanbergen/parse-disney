486a4b6058da1d38247a6dafd5cd7e73679ba4f5
Dirk van Bergen <dirk.van.bergen@waternet.nl>
2020-05-14 12:29:32 +0200

    Add project files.

---
+++ parse-bits/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace parse_bits
+{
+    class Program
+    {
+        static int MaxChapter = 18;
+        static int MaxStaminaCost = 12;
+        static int[] Points = new[] { 0, 0, 0, 1500, 1280, 0 };
+        static void Main(string[] args)
+        {
+            // https://wakkatu.github.io/dhero/en/item_list.htm
+            var req = WebRequest.CreateHttp("https://wakkatu.github.io/dhero/en/item_list.htm");
+            var res = req.GetResponse();
+            var reader = new StreamReader(res.GetResponseStream());
+            var input = reader.ReadToEnd().Split('\n');
+
+            var levels = new List<Level>();
+
+            var addingCampaigns = false;
+            Reward currentBit = null;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var line = input[i];
+                if (line.Contains("item-row-SHARD_"))
+                {
+                    var rarity = ParseRarity(input[i + 1]);
+                    var name = ParseName(input[i + 4]);
+                    i += 9;
+                    addingCampaigns = true;
+                    currentBit = new Reward { Name = name, Rarity = rarity };
+                }
+                else if (addingCampaigns && line.Contains("<li>Campaign "))
+                {
+                    int chapter = 0;
+                    int station = 0;
+                    ParseCampaign(line, out chapter, out station);
+                    if (chapter > MaxChapter) continue;
+
+                    var level = levels.FirstOrDefault(l => l.ChapterNumber == chapter && l.StationNumber == station);
+                    if (level == null)
+                    {
+                        level = new Level { ChapterNumber = chapter, StationNumber = station, StaminaCost = GetStamCostForChapter(chapter) };
+                        levels.Add(level);
+                    }
+                    level.Rewards.Add(currentBit);
+                    var totalPointsForLevel = level.Rewards.Sum(r => Points[(int)r.Rarity]);
+                    var pointsPerRaid = totalPointsForLevel / 4;
+                    var pointsPerStamina = pointsPerRaid / level.StaminaCost;
+                    level.ContestPointsPerStamina = pointsPerStamina;
+                }
+                else
+                {
+                    addingCampaigns = false;
+                }
+            }
+
+            foreach (var level in levels.OrderBy(l => l.ContestPointsPerStamina).ThenBy(l => l.ChapterNumber).ThenBy(l => l.StationNumber))
+            {
+                Console.WriteLine($"{level.ChapterNumber}-{level.StationNumber} PPS: {level.ContestPointsPerStamina} [Orange: {level.Rewards.Count(r => r.Rarity == Rarity.Orange)}, Purple: {level.Rewards.Count(r => r.Rarity == Rarity.Purple)}]");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int GetStamCostForChapter(int c)
+        {
+            if (c > MaxChapter) return int.MaxValue;
+
+            return Math.Max(6, MaxStaminaCost - (2 * (MaxChapter - c)));
+        }
+
+        static Rarity ParseRarity(string line)
+        {
+            if (line.Contains("rarity-orange")) return Rarity.Orange;
+            if (line.Contains("rarity-purple")) return Rarity.Purple;
+            if (line.Contains("rarity-blue")) return Rarity.Blue;
+            if (line.Contains("rarity-green")) return Rarity.Green;
+            if (line.Contains("rarity-red")) return Rarity.Red;
+            return Rarity.White;
+        }
+
+        static string ParseName(string line)
+        {
+            var parts = line.Split(new[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            return parts[3];
+        }
+
+        static void ParseCampaign(string line, out int chapter, out int station)
+        {
+            var parts = line.Split(new[] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            var parts2 = parts[2].Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            chapter = int.Parse(parts2[1]);
+            station = int.Parse(parts2[2]);
+        }
+    }
+
+    public class Level
+    {
+        public int ChapterNumber { get; set; }
+        public int StaminaCost { get; set; }
+        public int StationNumber { get; set; }
+        public int ContestPointsPerStamina { get; set; }
+        public List<Reward> Rewards { get; set; }
+
+        public Level()
+        {
+            Rewards = new List<Reward>();
+        }
+    }
+
+    public class Reward
+    {
+        public string Name { get; set; }
+        public Rarity Rarity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Rarity})";
+        }
+    }
+
+    public enum Rarity
+    {
+        White = 0,
+        Green,
+        Blue,
+        Purple,
+        Orange,
+        Red
+    }
+}
---
+++ parse-friends/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace parse_friends
+{
+    class Program
+    {
+        private static List<KeyValuePair<string, string>> Missions;
+        private static List<KeyValuePair<string, string>> Friendships;
+
+        private static List<string> IgnoreHeroes = new List<string> { "", "Tron", "Animal", "Miss Piggy", "Randall Boggs", "Linguini & Remy", "Powerline", "Hank & Dory", "Gerald, Marlin, & Nemo", "Tigger", "Winnie the Pooh", "Belle", "Eeyore", "Goliath", "The Evil Queen", "Syndrome" };
+        private static List<KeyValuePair<string, string>> IgnoreFriendships = new List<KeyValuePair<string, string>> {
+            new KeyValuePair<string, string>("Gonzo", "Anger"),
+            new KeyValuePair<string, string>("Gizmoduck", "Launchpad McQuack"),
+            new KeyValuePair<string, string>("Flynn Rider", "Aladdin"),
+            new KeyValuePair<string, string>("Megavolt", "Finnick"),
+            new KeyValuePair<string, string>("Kida", "Hercules"),
+            new KeyValuePair<string, string>("Kida", "Jasmine"),
+            new KeyValuePair<string, string>("Kristoff & Sven", "Kida"),
+            new KeyValuePair<string, string>("Kristoff & Sven", "Flynn Rider"),
+        };
+
+        static void Main(string[] args)
+        {
+            // https://wakkatu.github.io/dhero/en/disk_list.htm
+            var req = WebRequest.CreateHttp("https://wakkatu.github.io/dhero/en/disk_list.htm");
+            var res = req.GetResponse();
+            var reader = new StreamReader(res.GetResponseStream());
+            var content = reader.ReadToEnd().Split('\n'); ;
+
+            Friendships = ParseFile(content);
+            Missions = new List<KeyValuePair<string, string>>();
+            OptimizeMissions();
+
+            var i = 1;
+            foreach (var couple in Missions)
+            {
+                Console.WriteLine("{2}: {0} - {1}", couple.Key, couple.Value, i++);
+            }
+        }
+
+        private static List<KeyValuePair<string, string>> AvailableFriendships()
+        {
+            var usedHeroes = Missions.SelectMany(m => new[] { m.Key, m.Value }).ToList();
+            return Friendships.Where(f => (usedHeroes.IndexOf(f.Key) + usedHeroes.IndexOf(f.Value)) < 0).ToList();
+        }
+
+        private static List<string> LeastAvailableHero()
+        {
+            var counts = new Dictionary<string, int>();
+            foreach (var hero in AvailableFriendships().SelectMany(f => new[] { f.Key, f.Value }))
+            {
+                if (counts.ContainsKey(hero))
+                {
+                    counts[hero]++;
+                }
+                else
+                {
+                    counts.Add(hero, 1);
+                }
+            }
+            return counts.OrderBy(x => x.Value).Select(x => x.Key).ToList();
+        }
+
+        private static List<string> GetFriends(string hero, bool onlyAvailableFriends = false)
+        {
+            if (onlyAvailableFriends)
+            {
+                var friendshipsByKey = AvailableFriendships().Where(f => f.Key == hero).Select(f => f.Value).ToList();
+                var friendshipsByVal = AvailableFriendships().Where(f => f.Value == hero).Select(f => f.Key).ToList();
+                return friendshipsByKey.Union(friendshipsByVal).ToList();
+            }
+            var allFriendshipsByKey = Friendships.Where(f => f.Key == hero).Select(f => f.Value).ToList();
+            var allFriendshipsByVal = Friendships.Where(f => f.Value == hero).Select(f => f.Key).ToList();
+            return allFriendshipsByKey.Union(allFriendshipsByVal).ToList();
+        }
+
+        private static void OptimizeMissions()
+        {
+            while (AvailableFriendships().Count > 0)
+            {
+                var hero = LeastAvailableHero().First();
+                var friends = GetFriends(hero, true);
+                var friend = LeastAvailableHero().First(h => friends.Contains(h));
+                if (Friendships.Any(f => f.Key == hero && f.Value == friend))
+                {
+                    Missions.Add(new KeyValuePair<string, string>(hero, friend));
+                }
+                else
+                {
+                    Missions.Add(new KeyValuePair<string, string>(friend, hero));
+                }
+            }
+        }
+
+        private static List<KeyValuePair<string, string>> ParseFile(string[] content)
+        {
+            var readDisk = false;
+            var diskLines = new List<string>();
+            var friends = new List<KeyValuePair<string, string>>();
+            var friendExpr = ">([a-zA-Z\\s-&;.,]*)<";
+            foreach (var line in content)
+            {
+                if (line.Contains(" Disk { "))
+                {
+                    readDisk = true;
+                    continue;
+                }
+                if (line.Contains(" Disk } "))
+                {
+                    if (diskLines.Count > 1)
+                    {
+                        var hero = diskLines[0];
+                        var friend = diskLines[1];
+                        if (!IgnoreFriendships.Any(f => f.Key == hero && f.Value == friend))
+                        {
+                            friends.Add(new KeyValuePair<string, string>(hero, friend));
+                        }
+                    }
+                    diskLines.Clear();
+                    readDisk = false;
+                    continue;
+                }
+                if (readDisk && line.Contains("<a href=\"./hero/"))
+                {
+                    var match = Regex.Match(line, friendExpr);
+                    var hero = match.Groups[1].Value.Replace("&amp;", "&");
+                    if (IgnoreHeroes.Any(h => hero == h)) continue;
+                    diskLines.Add(hero);
+                }
+            }
+
+            return friends;
+        }
+
+
+    }
+}
